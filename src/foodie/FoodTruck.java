ba9ab144356ee99fcb7ce08dd31e6818c660ec6d
George Moore <gdubya1218@gmail.com>
2019-11-23 11:49:59 -0700

    fields getters setters constructors

--- src/foodie/FoodTruck.java
+++ src/foodie/FoodTruck.java
@@ -1,7 +1,65 @@
 package foodie;
 
 public class FoodTruck {
-	public int numId;
 	public String name;
+	public String foodType;
 	public double rating;
+	public int numId;
+	
+	
+	public FoodTruck(String name, String foodType, double rating) {
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+		
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+
+	public double getRating() {
+		return rating;
+	}
+
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+
+	public int getNumId() {
+		return numId;
+	}
+
+
+	public void setNumId(int numId) {
+		this.numId = numId;
+	}
+
+
+	@Override
+	public String toString() {
+		return "FoodTruck [name=" + name + ", foodType=" + foodType + ", rating=" + rating + ", numId=" + numId + "]";
+	}
+	
+	
+	
 }
--- src/foodie/FoodTruckReview.java
+++ src/foodie/FoodTruckReview.java
@@ -1,9 +1,27 @@
 package foodie;
 
+import java.util.Scanner;
+
 public class FoodTruckReview {
 
 	public static void main(String[] args) {
-		FoodTruck[] foodTrucks =  new FoodTruck[5];
+		FoodTruckReview ftr = new FoodTruckReview();
+		ftr.run();
+	}
+
+	public void run() {
+		System.out.println("Please input the name, food type, and rating for up to five food trucks.");
+		name = sc.next();
+		
+		FoodTruck[] truck = new FoodTruck[5];
+		for (int i = 1; i <= truck.length; i++) {
+			truck[i] = new FoodTruck(name, foodType, rating);
+			
+		}
+
+		Scanner sc = new Scanner(System.in);
+
+
 	}
 
 }
