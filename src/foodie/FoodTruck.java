82f22db4d12a3a80e155ee2c2e2586a476c7b06c
George Moore <gdubya1218@gmail.com>
2019-11-23 10:26:19 -0700

    created structure

--- src/foodie/FoodTruck.java
+++ src/foodie/FoodTruck.java
@@ -1,5 +1,7 @@
 package foodie;
 
 public class FoodTruck {
-
+	public int numId;
+	public String name;
+	public double rating;
 }
---
+++ src/foodie/FoodTruckReview.java
@@ -0,0 +1,9 @@
+package foodie;
+
+public class FoodTruckReview {
+
+	public static void main(String[] args) {
+		FoodTruck[] foodTrucks =  new FoodTruck[5];
+	}
+
+}
