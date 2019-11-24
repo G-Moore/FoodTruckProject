6c56dfb3d7bece30c5de279c9af8820b8f3dd55f
George Moore <gdubya1218@gmail.com>
2019-11-24 08:07:31 -0700

    fully functioning program

--- src/foodie/FoodTruckReview.java
+++ src/foodie/FoodTruckReview.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class FoodTruckReview {
 	int n = 5;
 	FoodTruck[] truck = new FoodTruck[n];
+	int nextTruckId = 0;
 
 	public static void main(String[] args) {
 		FoodTruckReview ftr = new FoodTruckReview();
@@ -13,10 +14,10 @@ public class FoodTruckReview {
 
 	public void run() {
 		Scanner sc = new Scanner(System.in);
-			int nextTruckId = 0;
+
 		for (int i = 0; i < truck.length; i++) {
 
-			nextTruckId = i +1;
+			nextTruckId = i + 1;
 
 			System.out.println("Please input the name of the Food truck: ");
 			String inName = sc.next();
@@ -36,7 +37,7 @@ public class FoodTruckReview {
 			System.out.println(truck[i]);
 			System.out.println(i);
 
-		} 
+		}
 
 		while (true) {
 
@@ -48,29 +49,35 @@ public class FoodTruckReview {
 
 			if (opt == 1) {
 				for (int i = 0; i < truck.length; i++) {
+					if(truck[i] != null)
 					System.out.println(truck[i].getName());
 				}
 			}
 			if (opt == 2) {
 				double sum = 0;
 				double avgRate;
+				int avgCount = 0;
 				for (int i = 0; i < truck.length; i++) {
+					if(truck[i] != null) {
 					sum += truck[i].getRating();
+					avgCount = i;
 				}
-				avgRate = sum / truck.length;
+				}
+				avgRate = sum / (truck[avgCount].getNumId()) ;
 				System.out.println(avgRate);
 			}
 			if (opt == 3) {
 				double hiRate = truck[0].getRating();
 				int count = 0;
 				for (int i = 0; i < truck.length; i++) {
+					if(truck[i] != null)
 					if (hiRate < truck[i].getRating()) {
 						hiRate = truck[i].getRating();
 						count = i;
 					}
 				}
-				System.out.println("The highest rated Food Truck is " + truck[count].getName()
-						+ "\nWith a rating of: "+ hiRate);
+				System.out.println(
+						"The highest rated Food Truck is " + truck[count].getName() + "\nWith a rating of: " + hiRate);
 			}
 			if (opt == 4) {
 				System.out.println("Thanks for using Food Truck Review. \n\tEat on Foodie!");
@@ -79,7 +86,4 @@ public class FoodTruckReview {
 		}
 		sc.close();
 	}
-	}
-
-	
-
+}
