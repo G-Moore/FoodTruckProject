8850b2120a40a55456763b3d1eb1e23c0702b2fd
George Moore <gdubya1218@gmail.com>
2019-11-24 08:54:20 -0700

    added methods to while loop

--- src/foodie/FoodTruckReview.java
+++ src/foodie/FoodTruckReview.java
@@ -33,57 +33,71 @@ public class FoodTruckReview {
 			int rate = sc.nextInt();
 
 			truck[i] = new FoodTruck(inName, fType, rate, nextTruckId);
-
-			System.out.println(truck[i]);
-			System.out.println(i);
-
 		}
 
 		while (true) {
-
-			System.out.println("Enter an option number." + "\n1 : List all existing food trucks names."
-					+ "\n2 : See the average rating of food trucks." + "\n3 : Display the highest-rated food truck."
-					+ "\n4 : Quit the Program.");
-
+			printMenu();
 			int opt = sc.nextInt();
 
 			if (opt == 1) {
-				for (int i = 0; i < truck.length; i++) {
-					if (truck[i] != null)
-						System.out.println(truck[i].getName());
-				}
+				printNames();
 			}
 			if (opt == 2) {
-				double sum = 0;
-				double avgRate;
-				int avgCount = 0;
-				for (int i = 0; i < truck.length; i++) {
-					if (truck[i] != null) {
-						sum += truck[i].getRating();
-						avgCount = i;
-					}
-				}
-				avgRate = sum / (truck[avgCount].getNumId());
-				System.out.println(avgRate);
+				printAvgRate();
 			}
 			if (opt == 3) {
-				double hiRate = truck[0].getRating();
-				int count = 0;
-				for (int i = 0; i < truck.length; i++) {
-					if (truck[i] != null)
-						if (hiRate < truck[i].getRating()) {
-							hiRate = truck[i].getRating();
-							count = i;
-						}
-				}
-				System.out.println(
-						"The highest rated Food Truck is " + truck[count].getName() + "\nWith a rating of: " + hiRate);
+				printHiRate();
 			}
 			if (opt == 4) {
-				System.out.println("Thanks for using Food Truck Review. \n\tEat on Foodie!");
+				printExit();
 				break;
 			}
 		}
 		sc.close();
+	}
+
+	private void printExit() {
+		System.out.println("Thanks for using Food Truck Review. \n\tEat on Foodie!");
+	}
+
+	private void printMenu() {
+		System.out.println("Enter an option number." + "\n1 : List all existing food trucks names."
+				+ "\n2 : See the average rating of food trucks." + "\n3 : Display the highest-rated food truck."
+				+ "\n4 : Quit the Program.");
+	}
+
+	private void printNames() {
+		for (int i = 0; i < truck.length; i++) {
+			if (truck[i] != null)
+				System.out.println(truck[i].getName());
+		}
+	}
+
+	private void printAvgRate() {
+		double sum = 0;
+		double avgRate;
+		int avgCount = 0;
+		for (int i = 0; i < truck.length; i++) {
+			if (truck[i] != null) {
+				sum += truck[i].getRating();
+				avgCount = i;
+			}
+		}
+		avgRate = sum / (truck[avgCount].getNumId());
+		System.out.println(avgRate);
+	}
+
+	private void printHiRate() {
+		double hiRate = truck[0].getRating();
+		int count = 0;
+		for (int i = 0; i < truck.length; i++) {
+			if (truck[i] != null)
+				if (hiRate < truck[i].getRating()) {
+					hiRate = truck[i].getRating();
+					count = i;
+				}
+		}
+		System.out
+				.println("The highest rated Food Truck is " + truck[count].getName() + "\nWith a rating of: " + hiRate);
 	}
 }
